901f7c83c14208d41dea684ca5d29f9c8416df82
欧贺福 <ouhefu@xiao100.com>
2020-08-31 09:24:47 +0800

    feat

---
+++ Python/wechat.py
@@ -0,0 +1,6 @@
+# 导入模块
+from wxpy import *
+# 初始化机器人，扫码登陆
+bot = Bot()
+
+
---
+++ tools/test-project/cli-plugin/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    build: function () {
+        console.log('test')
+    },
+    config: {
+        buildOptions: ['test', 'pre', 'prd']
+    }
+}
